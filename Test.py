72eda2479133fbff96c9bce127836db2da5eb8ef
chenllliang <547205480@qq.com>
2020-04-22 21:57:58 +0800

    退出程序时关闭所有线程

--- Test.py
+++ Test.py
@@ -12,18 +12,25 @@ class Recorder(QMainWindow,Ui_MainWindow):
         self.setupUi(self)
         self.output = QFile()
         self.audio = QAudioInput()
+        self.NEED_ASR = False
+        self.Open = True
+        
+
 
         self.th1 = threading.Thread(target=self.ASR)
+        self.th1.start()
 
         self.Pause_Button.clicked.connect(self.toggle_pause)
         self.Record_Button.clicked.connect(self.toggle_record)
 
+    def closeEvent(self,event):
+        self.Open = False
 
 
     def toggle_record(self):
         if self.Record_Button.text()=="开始录音":
             self.statusbar.showMessage("开始录音")
-            self.th1._stop()
+            self.setup()
             self.audio.start(self.output)
             self.Record_Button.setText("停止")
             self.Pause_Button.setText("暂停")
@@ -31,15 +38,10 @@ class Recorder(QMainWindow,Ui_MainWindow):
         else:
             self.audio.stop()
             self.output.close()
-
-            self.audio = QAudioInput()
-            self.output = QFile()
-            self.setup()
-
             self.Pause_Button.setText("暂停")
             self.Record_Button.setText("开始录音")
             self.statusbar.showMessage("正在使用百度智能云识别，请稍等")
-            self.th1.start()
+            self.NEED_ASR = True
 
     def toggle_pause(self):
         if self.Pause_Button.text()=="暂停":
@@ -51,16 +53,24 @@ class Recorder(QMainWindow,Ui_MainWindow):
             self.audio.resume()
 
     def ASR(self):
-        try:
-            text = ASR().Baidu_ASR("record.pcm")
-            print(text)
-            if text['err_msg'] == 'success.':
-                self.Text_plainTextEdit.setPlainText(text['result'][0])
-                self.statusbar.showMessage("识别成功")
+        while self.Open:
+            print(1)
+            if self.NEED_ASR:
+                try:
+                    text = ASR().Baidu_ASR("record.pcm")
+                    print(text)
+                    if text['err_msg'] == 'success.':
+                        self.Text_plainTextEdit.setPlainText(text['result'][0])
+                        self.statusbar.showMessage("识别成功")
+
+                    else:
+                        self.statusbar.showMessage("识别失败,请再说一遍")
+                except:
+                    self.statusbar.showMessage("网络故障，请检查网络设置以及百度智能云账号")
+
+                self.NEED_ASR = False
             else:
-                self.statusbar.showMessage("识别失败,请再说一遍")
-        except:
-            self.statusbar.showMessage("网络故障，请检查网络设置以及百度智能云账号")
+                pass
 
 
 
@@ -93,4 +103,5 @@ if __name__=='__main__':
     x.show()
 
     sys.exit(app.exec_())
+
 
